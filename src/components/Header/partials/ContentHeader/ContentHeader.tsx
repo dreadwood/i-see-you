c680247c6db90d871ccf4c7095790737f2ffc737
srg <skabassjob@gmail.com>
2024-09-27 01:04:28 +0400

    refactor: remove SocialLink

--- src/components/Header/partials/ContentHeader/ContentHeader.tsx
+++ src/components/Header/partials/ContentHeader/ContentHeader.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react'
 import Icon from '@/components/Icon/Icon'
 import SocialLinks from '@/components/SocialLink/SocialLink'
 import Overlay from '@/UI/Overlay/Overlay'
-import { headerLinks, SocialLink } from '@/app/const'
+import { headerLinks } from '@/app/const'
 import { Link } from '@/services/strapi/types'
 
 import styles from './ContentHeader.module.scss'
